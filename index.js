807c2fe09c3b2026bf5097857409a1aac1c9955b
STS-Engineer <administration.sts@avocarbon.com>
2025-04-04 22:03:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 // Use cors middleware
 // Allow only requests from http://example.com
-app.use(cors({
-    origin: 'http://4.211.132.216:3000'
-  }));
+//app.use(cors({
+    //origin: 'http://4.211.132.216:3000'
+  //}));
 
 
 app.use('/companies', companyRouter);
 
 //const port = process.env.PORT || 4000;
-app.listen(8181);
+app.listen(8080);
